501be7a9ce798af99d512b47261ed48197f42a1d
haseomg <lovethispool@kakao.com>
2023-01-04 15:12:02 +0900

    [recyclerview delete] add

--- app/src/main/java/com/example/memoexam/MainActivity.java
+++ app/src/main/java/com/example/memoexam/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -41,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate()");
-
         mainLogo = findViewById(R.id.mainText);
+
 
 
 
